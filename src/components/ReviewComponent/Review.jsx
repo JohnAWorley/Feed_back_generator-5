c1119fb9a0526e4b2333d23b9a8fcd6f71ebbd21
John Worley <john.aver.worley@gmail.com>
2018-12-09 17:52:11 -0600

    review page posting information

--- src/components/ReviewComponent/Review.jsx
+++ src/components/ReviewComponent/Review.jsx
@@ -1,12 +1,21 @@
-import React, {Component} from 'react';
-// import { connect } from 'react-redux';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+const mapReduxStateToProps = (reduxState) => ({
+    reduxState
+})
 
 class Review extends Component {
     render() {
-        return(
-            <h1>Review</h1>
+        return (
+            <div>
+                <p>Your current feeling: {this.props.reduxState.buttonReducer.feeling}</p>
+                <p>Your current understanding: {this.props.reduxState.buttonReducer.understanding} </p>
+                <p>Your current support: {this.props.reduxState.buttonReducer.supported}</p>
+                <p>Additional comments: {this.props.reduxState.buttonReducer.comments}</p>
+            </div>
         )
     }
 }
 
-export default Review;
+export default connect(mapReduxStateToProps)(Review);
