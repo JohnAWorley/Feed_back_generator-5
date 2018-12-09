717c3af82cda88e107f9fa02bb7c2df65f96c27f
John Worley <john.aver.worley@gmail.com>
2018-12-08 22:21:52 -0600

    basic index setup

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+// import axios from 'axios';
 import './App.css';
 
-import { HashRouter as Router, Route, Link } from 'react-router-dom'
+import { HashRouter as Router, Route } from 'react-router-dom'
 import Comments from '../CommentsComponent/Comments'
 import Feeling from '../FeelingComponent/Feeling';
 import Header from '../HeaderComponent/Header';
--- src/index.js
+++ src/index.js
@@ -3,9 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
-// import {Provider} from 'react-redux';
-// import {createStore, combineReducers, applyMiddleware} from 'react';
+import { Provider } from 'react-redux';
+import {createStore, combineReducers, applyMiddleware} from 'redux';
+import logger from 'redux-logger';
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const selectPizzaReducer = (state = [], action) => {
+    switch (action.type) {
+        case 'ADD_PIZZA': return action.payload;
+        default: return state
+    }
+}
+
+const storeInstance = createStore(
+    combineReducers({
+        selectPizzaReducer
+    }),
+    applyMiddleware(logger)
+)
+
+
+ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
